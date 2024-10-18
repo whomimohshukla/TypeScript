abc6e123697e5ec477780ffd7cb500e9ecb494c8
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-10-18 22:10:46 +0530

    functions.ts

---
+++ Basics/fnction.ts
@@ -0,0 +1,13 @@
+function addTwo(num: number) {
+  return num + 2;
+}
+
+console.log(addTwo(10));
+
+function getUpper(params: string) {
+  return params.toUpperCase();
+}
+
+console.log(getUpper("hello"));
+
+export {};
