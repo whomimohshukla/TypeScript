cdf1b67c3db67adc04b8e985a2d0ce9b8b80ecb0
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-12-28 16:06:03 +0530

    feat: add type annotations for chainName variable in typeanootatins.ts

---
+++ typeanootatins.ts
@@ -0,0 +1,20 @@
+// let drink="Water";
+
+
+// drink=10; // Error: Type 'number' is not assignable to type 'string'
+
+
+// let channelName="Channel 1";
+
+// channelName=10; // Error: Type 'number' is not assignable to type 'string'
+
+
+
+// type anootation
+
+
+let chainName: string = "Channel 1";
+
+chainName = "New Channel";
+
+// chainName=10; // Error: Type 'number' is not assignable to type 'string'
