d91abe412e14717878d066ec7d60270f76e51da1
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-12-28 16:05:33 +0530

    refactor: comment out unused code and examples in array.ts

--- array.ts
+++ array.ts
@@ -1,43 +1,68 @@
-let number: number[] = [1, 2, 3, 4, 5];
+// let number: number[] = [1, 2, 3, 4, 5];
 
-console.log(number);
+// console.log(number);
 
-let strings: string[] = ["Hello", "TS"];
-console.log(strings);
+// let strings: string[] = ["Hello", "TS"];
+// console.log(strings);
 
-// let mixed: number | string[] = [1, "Hello"];
-// console.log(mixed);
+// // let mixed: number | string[] = [1, "Hello"];
+// // console.log(mixed);
 
-// using type alias for Array
+// // using type alias for Array
 
-type User = {
+// type User = {
+// 	name: string;
+// 	age: number;
+// };
+
+// let users: User[] = [
+// 	{
+// 		name: "Mimoh",
+// 		age: 23,
+// 	},
+// 	{
+// 		name: "rohit",
+// 		age: 25,
+// 	},
+// ];
+
+// console.log(users);
+
+// // Generic Array Function
+
+// function getFirst<T>(arr: T[]): T {
+//   return arr[0];
+// }
+
+// console.log(getFirst<number>([10, 20]));  // 10
+// console.log(getFirst<string>(["a", "b"])); // "a"
+
+// export {};
+
+// const chaiFalavour:string[] = ["Chai", "Falavour", "Chai Tea"];
+
+// const chaiFalavour2:Array<string> = ["Masala", "Ginger", "Lemon"];
+
+const chaiNaam1: string[] = ["Chai", "Falavour", "Chai Tea"];
+const chaiNaam2: Array<string> = ["Masala", "Ginger", "Lemon"];
+console.log(chaiNaam1);
+console.log(chaiNaam2);
+
+type chai = {
 	name: string;
-	age: number;
+	price: number;
+	isHotDrink: boolean;
 };
 
-let users: User[] = [
-	{
-		name: "Mimoh",
-		age: 23,
-	},
-	{
-		name: "rohit",
-		age: 25,
-	},
+let chais: chai[] = [
+	{ name: "Chai", price: 100, isHotDrink: true },
+	{ name: "Falavour", price: 200, isHotDrink: false },
+	{ name: "Chai Tea", price: 300, isHotDrink: true },
 ];
 
-console.log(users);
+// tuples
 
+let chaiTuple:[string,number];
 
+chaiTuple = ["Masala Chai",150];
 
-
-// Generic Array Function
-
-function getFirst<T>(arr: T[]): T {
-  return arr[0];
-}
-
-console.log(getFirst<number>([10, 20]));  // 10
-console.log(getFirst<string>(["a", "b"])); // "a"
-
-export {};
