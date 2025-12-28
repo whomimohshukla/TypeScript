3b0e5bf0f25636dfaa4da6a87520114f67e96ad3
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-12-28 16:06:53 +0530

    feat: add union type examples for subscribers, apiResponse, and airLineSeat

---
+++ typeNarrowing.ts
@@ -0,0 +1,5 @@
+
+// function getChai(kind:string | number){
+//     if(typeof kind === "string"){
+//         return `Making ${kind} tea`;
+// }
---
+++ union.ts
@@ -0,0 +1,6 @@
+let subscribers: number | string = 1000;
+
+let apiResponse: boolean | string = "Success";
+
+let airLineSeat: "aisle" | "window" | "middle" = "aisle";
+
